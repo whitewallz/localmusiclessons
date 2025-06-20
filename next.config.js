ed0823ad315f28098627495b81b3e6ce686e19a5
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-19 20:59:40 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any custom Next.js config here, for example:
+  // images: { domains: ['your-image-domain.com'] },
+  // env: { CUSTOM_VAR: process.env.CUSTOM_VAR },
+};
+
+module.exports = nextConfig;
