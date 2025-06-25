900c6b84b10066a364b34283cc0cff734b3e025d
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-25 02:10:30 -0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-}
+  apiKey: "AIzaSyCowdIHhjf8WyiUc_xkXYtCud5DW75SIn8",
+  authDomain: "localmusiclessons-ff87b.firebaseapp.com",
+  projectId: "localmusiclessons-ff87b",
+  storageBucket: "localmusiclessons-ff87b.firebasestorage.app",
+  messagingSenderId: "182373655384",
+  appId: "1:182373655384:web:abd9fccf03f42382e07b2d",
+  measurementId: "G-YK5PF8S253"
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
