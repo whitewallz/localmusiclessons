54118ebb224b6147769ad2501b4dca69b16b7a71
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-25 09:37:42 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,7 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 import Layout from '../components/Layout'
-// ...
-return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-)
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -19,11 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Navbar />
-      <main className="min-h-screen px-4 sm:px-8 md:px-12">
+      <Layout>
         <Component {...pageProps} />
-      </main>
-      <Footer />
+      </Layout>
     </>
   )
 }
