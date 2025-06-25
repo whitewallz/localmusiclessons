1460b0d5c663cb8dc1d8c99afd240ff32f2ed983
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-25 02:16:21 -0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,8 +10,8 @@ const firebaseConfig = {
   storageBucket: "localmusiclessons-ff87b.firebasestorage.app",
   messagingSenderId: "182373655384",
   appId: "1:182373655384:web:abd9fccf03f42382e07b2d",
-  measurementId: "G-YK5PF8S253"
-
+  measurementId: "G-YK5PF8S253",
+}
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
