c89e587843c7e52ca54f8cbb2679b1741c70eef0
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-25 02:06:23 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,3 +5,5 @@ export default function Footer() {
 <footer className="text-center text-sm text-gray-500 mt-12 p-6">
   © {new Date().getFullYear()} LocalMusicLessons. All rights reserved.
 </footer>
+  )
+}
