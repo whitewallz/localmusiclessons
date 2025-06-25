feb4abf982c1251fb65c8a620bfaaf0408a5ec02
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-25 01:23:36 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen px-4 sm:px-8 md:px-12">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  )
 }
