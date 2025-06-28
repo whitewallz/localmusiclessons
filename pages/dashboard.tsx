c154eb5f1b3f9d6ef493c69c887f8ae37374cc5e
Robert McKellar <157163567+whitewallz@users.noreply.github.com>
2025-06-27 23:52:20 -0600

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -62,6 +62,7 @@ export default function Dashboard() {
               phone: '',
               pricing: '',
               lessonType: '',
+              location: '',
             })
           }
         } catch (err) {
